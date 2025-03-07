1bd1b9eaa3b4d59900b4a5b029ee968137c47ce8
Borjan40 <andrey40cadrus@gmail.com>
2025-03-07 23:13:15 +0300

    react-level2-2 time 39:40 стратегия раздачи статики мы не хотим все содержимое dist хостить на прямую, поэтому раздаем статику: -скрипту из папки assets, -картинке favicon.ico

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ import { renderToString } from "react-dom/server";
 import express from "express";
 import { readFileSync } from "fs";
 import { url } from "inspector";
-// import createApp from './src/app';
 
 const server = express();
 const template = readFileSync("./dist/index.html").toString("utf-8");
 
-// server.use(express.static('dist'))
+server.use("/assets", express.static("dist/assets"));
+server.use("/favicon.ico", express.static("dist/favicon.ico"));
 
 server.get("*", async function (req, resp) {
   const context = { url: req.url };
