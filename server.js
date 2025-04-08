5139d3376336872abd6c18d9455bd56096c9065a
Borjan40 <andrey40cadrus@gmail.com>
2025-04-08 11:23:30 +0300

    react-level2-4 time 40:00 техника работы с промисом по касательной (смотри README.MD)

--- server.js
+++ server.js
@@ -12,12 +12,23 @@ server.use(/.*\.js$/, function (_, resp) {
 server.use("/assets", express.static("dist/assets"));
 server.use("/favicon.ico", express.static("dist/favicon.ico"));
 
+async function recoursiveRender(app, cashe) {
+  const html = renderToString(app);
+const awaitingArr = Object.entries(cashe.awaiting)
+
+if(awaitingArr.length > 0) {
+console.log(await Promise.all(awaitingArr.map(([k, p]) => p.then(data =>({k, data})))))
+}
+  return html;
+
+}
+
 server.get("*", async function (req, resp) {
   try {
     const context = { url: req.url };
     const { app, store, cashe } = await createApp(context);
     console.log(cashe);
-    const html = renderToString(app);
+    const html = await recoursiveRender(app, cashe);
     console.log(cashe);
     const page = template
       .replace("<!--ssr-->", html)
